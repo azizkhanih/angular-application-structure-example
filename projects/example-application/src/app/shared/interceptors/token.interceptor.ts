894d2427584cc4acf59a4f8aa460d3fac46f01a2
h.azizkhani <azizkhani.develop@gmail.com>
2021-11-27 00:56:14 +0330

    feat: add interceptores, add account, shared and layout module

---
+++ projects/example-application/src/app/shared/interceptors/token.interceptor.ts
@@ -0,0 +1,29 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'projects/example-application/src/environments/environment';
+import { Observable } from 'rxjs';
+import { AccountService } from '../../account/services/account.service';
+
+@Injectable()
+export class TokenInterceptor implements HttpInterceptor
+{
+  constructor(private accountService: AccountService) { }
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>>
+  {
+    const account = this.accountService.accountValue;
+    const isLoggedIn = account?.accessToken;
+    const isApiUrl = request.url.startsWith(environment.apiUrl);
+
+    if (isLoggedIn && isApiUrl)
+    {
+      request = request.clone({
+        setHeaders: {
+          authorization: `Bearer ${ account.accessToken }`
+        }
+      });
+    }
+
+    return next.handle(request);
+  }
+}
